bf172da7773650ab86eb4999ad2c2d70863c4be1
skgm1992 <shubhj.1588@gmail.com>
2026-01-08 16:18:35 +0530

    new db url added

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 const PORT = 3000 || process.env.PORT;
-const DB_URL = "mongodb+srv://sirvaiyarakesh18:sobgxQIAhJ5NmEXf@cluster0.qzi2gwt.mongodb.net";
+const DB_URL = "mongodb+srv://shubhj1588:1r8Afc1L7Mlrj4sN@cluster0.td0mwwn.mongodb.net/";
 
 export {PORT,DB_URL}
